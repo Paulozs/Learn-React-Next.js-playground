51bb89bf6cab683a91c858cbdadf0f93cc2c8cca
Paul Ong <paulongzs@gmail.com>
2023-05-21 19:28:27 +0800

    Added Sidebar and navbar

--- my-t3-app/src/server/api/routers/example.ts
+++ my-t3-app/src/server/api/routers/example.ts
@@ -7,51 +7,21 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const itemRouter = createTRPCRouter({
-  additem: publicProcedure
-   .input(
-    z.object({ 
-      name: z.string(),
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
     }),
-   )
-   .mutation(async({ ctx, input })=> {
-    try{ 
-      await ctx.prisma.shoppingItem.create({
-        data:{
-          name,
-          
-        },
-      });
-    } catch(error){
-      console.log(error);
-    }
-   })
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
 })
-
-//  .mutation(async ({ input, ctx }) => {
-  //     const { name }= input
-  //     await ctx.prisma.shoppingItem.create({
-  //       data:{
-  //         name,
-  //         checked: false
-  //       },
-  //   },
-  //   )}
-
-// export const exampleRouter = createTRPCRouter({
-//   hello: publicProcedure
-//     .input(z.object({ text: z.string() }))
-//     .query(({ input }) => {
-//       return {
-//         greeting: `Hello ${input.text}`,
-//       };
-//     }),
-
-//   getAll: publicProcedure.query(({ ctx }) => {
-//     return ctx.prisma.example.findMany();
-//   }),
-
-//   getSecretMessage: protectedProcedure.query(() => {
-//     return "you can now see this secret message!";
-//   }),
-// })
