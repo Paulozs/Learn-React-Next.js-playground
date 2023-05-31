b1d2a3457f98a54e3c4d2409b7a4bd537b23e210
Paul Ong <paulongzs@gmail.com>
2023-05-31 21:19:28 +0800

    improved the NewTweetForm.tsx and included profile image capabilities, need to include authentication for the features to be fully implemented. Paused development, too many unknowns when dealing with code writing and css tailwind

--- my-t3-app/src/components/NewTweetForm.tsx
+++ my-t3-app/src/components/NewTweetForm.tsx
@@ -1,8 +1,54 @@
-export function NewTweetForm() {
+import { useSession } from "next-auth/react";
+import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
+
+function Form() {
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2 ">
-      <div className="flex gap-4">{/* <ProfileImage src="#url" /> */}</div>
-      <button>random stuff</button>
+      <div className="flex gap-4">
+        {/* <ProfileImage src={session.data.user.image} /> */}
+        <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="what's happening this is a textbox!!!"
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+        />
+      </div>
+      <Button className="self-end">Tweet</Button>
     </form>
   );
+}
+export function NewTweetForm() {
+  const session = useSession();
+  // this is saying if you are authenticated do not show NewTweetForm
+  // need to adjust when adding auth
+  if (session.status === "authenticated") return null;
+
+  return <Form />;
 }
